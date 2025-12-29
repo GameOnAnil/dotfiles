f5a2de1a2e3f1e93b776f1bf9b11f1f55e8fbaa0
Anil Thapa <anil.thapa@8squarei.com>
2025-12-29 14:50:35 +0545

    feat(zsh): add new aliases for branch cleanup and iOS deep clean

--- zsh/.aliases.zsh
+++ zsh/.aliases.zsh
@@ -2,3 +2,5 @@
 alias zconfig="code ~/.zshrc"
 alias fcl="flutter clean && flutter pub get"
 alias fgen="flutter pub run build_runner build --delete-conflicting-outputs"
+alias branch_clean="git branch --merged | egrep -v '(^\*|master|dev)' | xargs git branch -d"
+alias ios_deep_clean="cd ios && rm -rf Pods && rm -rf Podfile.lock && rm -rf ~/.pub-cache/hosted/pub.dartlang.org/ && pod cache clean --all  && arch -x86_64 pod repo update && arch -x86_64 pod install && cd .."
